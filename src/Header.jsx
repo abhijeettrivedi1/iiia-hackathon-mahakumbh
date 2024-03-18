3ef650cad9644ea467c62bb1191fa002f79dcdc8
harshitnitjsr <harshit26092004@gmail.com>
2024-03-18 17:01:00 +0530

    minor fixes

--- src/Header.jsx
+++ src/Header.jsx
@@ -10,12 +10,13 @@ export default function Header() {
                 {/* <Link to={"/about"} className="text-white hover:text-red-900 transition duration-300 ease-in-out ml-4 text-lg">About</Link> */}
             </div>
             <div className="flex items-center mr-4">
-                <Link to={"/login"} className="text-white hover:text-red-900 transition duration-300 ease-in-out mr-4 text-lg">
-                    Login
+                <Link to={"/face"} className="text-white hover:text-red-900 transition duration-300 ease-in-out mr-4 text-lg">
+                    Alert System 
                 </Link>
-                <Link to={"/register"} className="text-white hover:text-red-900 transition duration-300 ease-in-out mr-4 text-lg">
-                    Register
+                <Link to={"/traffic"} className="text-white hover:text-red-900 transition duration-300 ease-in-out mr-4 text-lg">
+                    Traffic Management
                 </Link>
+                
                 
             </div>
         </nav>
